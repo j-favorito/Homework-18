3bb9c845571f082cb6ead7edb81b08a24232f153
J Fav <jfavorito@outlook.com>
2020-07-06 16:29:35 -0700

    condensed js files into one bundle.js

---
+++ public/assets/src/entry.js
@@ -0,0 +1,2 @@
+import '../js/db';
+import '../js/index';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 const config = {
-  entry: "./public/assets/js/index.js",
+  entry: "./public/assets/src/entry.js",
   output: {
     path: __dirname + "/public/dist",
     filename: "bundle.js"
   },
   mode: "production"
 }
+
+
 
 module.exports = config;
